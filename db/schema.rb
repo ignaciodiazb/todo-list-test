8787ef51402e318c909d6e550134cb2a61015bf9
Ignacio Díaz <ignacio.db@icodesx.com>
2025-09-07 19:53:45 -0300

    feat: add association between Todo and User
    - Add relation between Todo and User models
    - Refactor Todo index view to improve accessibility

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_07_162238) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_07_191209) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -27,6 +27,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_07_162238) do
     t.integer "estimated_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_07_162238) do
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "todos", "users"
 end
